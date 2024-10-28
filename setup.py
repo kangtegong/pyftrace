ed30a260b7e4db2a22141407d7d2632630776721
Kang Minchul <tegongkang@gmail.com>
2024-10-28 12:55:05 +0900

    Add -v, --version option
    This commit adds -v, --version to pyftrace
    which can be used as follows:
    
     $ pyftrace -v
     pyftrace version 0.1.0
     $ pyftrace --version
     pyftrace version 0.1.0

--- setup.py
+++ setup.py
@@ -1,8 +1,21 @@
 from setuptools import setup, find_packages
+import os
+import sys
+
+def get_version():
+    here = os.path.abspath(os.path.dirname(__file__))
+    init_path = os.path.join(here, 'pyftrace', '__init__.py')
+    with open(init_path, 'r') as f:
+        for line in f:
+            if line.startswith('__version__'):
+                delim = '"' if '"' in line else "'"
+                version = line.split(delim)[1]
+                return version
+    raise RuntimeError("Unable to find version string.")
 
 setup(
     name="pyftrace",
-    version="0.1.0",
+    version=get_version(),
     description="Python function tracing tool.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
